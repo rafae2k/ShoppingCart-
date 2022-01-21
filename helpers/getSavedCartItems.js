d162c1cffae21dafe0e2fefc214a8a9fe9c225f7
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-01-21 13:09:54 -0300

    localStorage Functions

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,5 +1,6 @@
 const getSavedCartItems = () => {
   // seu código aqui
+  return localStorage.getItem('cartItems');
 };
 
 if (typeof module !== 'undefined') {
--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,5 @@
-const saveCartItems = () => {
-  // seu código aqui
+const saveCartItems = (item) => {
+  localStorage.setItem('cartItems', `${item}`);
 };
 
 if (typeof module !== 'undefined') {
