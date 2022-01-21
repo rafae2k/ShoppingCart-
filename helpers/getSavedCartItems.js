174947ee5babe996151027b1d995204ca5582010
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-01-21 16:21:24 -0300

    fix: lint

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,7 +1,4 @@
-const getSavedCartItems = () => {
-  // seu código aqui
-  return localStorage.getItem('cartItems');
-};
+const getSavedCartItems = () => localStorage.getItem('cartItems');
 
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
