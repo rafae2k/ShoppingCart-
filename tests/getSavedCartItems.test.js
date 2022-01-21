2e9fa7b1ba47139d1a82fd7d10d006b3d7bb58bc
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-01-21 13:08:46 -0300

    10. Desenvolva testes de no mínimo 75% de cobertura total e 100% da função saveCartItems

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,5 +5,14 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('1. Ao executar getSavedCartItems, o método localStorage.getItem é chamado', () => {
+    expect.assertions(1);
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  it('2. Ao executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro.', () => {
+    expect.assertions(1);
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
