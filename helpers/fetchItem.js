afa159213cd7770eed99fbbcf189564136ac18bc
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-01-21 11:50:51 -0300

    refactoring(fetchItem): remove $ from query URL

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (id) => {
+  const productId = `https://api.mercadolibre.com/items/${id}`;
+
+  try {
+    const response = await fetch(productId);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('Produto não encontrado');
+  }
 };
 
 if (typeof module !== 'undefined') {
